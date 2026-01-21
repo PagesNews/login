8f694af09934060524b33a6764e0f8aee17635fa
PagesNews <maironbaquero30@gmail.com>
2026-01-21 12:20:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 console.log("JS conectado correctamente");
 
+// 1. Capturamos el botón
+const btnRegistro = document.getElementById("btnRegistro");
+
+// 2. Verificamos que exista
+if (btnRegistro) {
+  btnRegistro.addEventListener("click", () => {
+    alert("✅ El botón ya funciona");
+    console.log("Botón presionado");
+  });
+} else {
+  console.error("❌ No se encontró el botón");
+}
+
+
